1a6afcc74f95835414926b36ac6d475775e70264
DeveloperDowny <vedantpanchal12345@gmail.com>
2022-12-08 22:35:12 +0530

    Update transfer.js

--- server/routes/transfer.js
+++ server/routes/transfer.js
@@ -43,3 +43,40 @@ router.post("/", async function (req, res, next) {
 });
 
 export default router;
+
+// DELIMITER $$
+// CREATE DEFINER=`rootdbms`@`%` PROCEDURE `checkAndTransact`(IN `sender_acc_num` INT, IN `receiver_acc_num` INT, IN `amount` INT, OUT `isSuccessful` VARCHAR(1))
+// mproc: BEGIN
+// SET autocommit=0;
+// 	set isSuccessful = 0;
+// 	select @sender_bal:= balance from cust_acc where acc_num = sender_acc_num;
+//      select @receiver_bal:= balance from cust_acc where acc_num = receiver_acc_num;
+//     select (@sender_bal);
+//     select (@sender_bal - amount);
+// 	IF @sender_bal - amount < 0 then
+//     set isSuccessful = 0;
+//     insert into cust_trans (cust_trans.sender_balance_snapshot,
+//                             cust_trans.receiver_balance_snapshot,
+//                             cust_trans.trans_status,
+//                             cust_trans.sender_acc_num,
+//                             cust_trans.receiver_acc_num, cust_trans.amount)
+//                             VALUES (@sender_bal, @receiver_bal, isSuccessful, sender_acc_num,receiver_acc_num, amount);
+//     COMMIT;
+//     leave mproc;
+//   end if;
+
+//   update cust_acc set balance = @sender_bal - amount where acc_num = sender_acc_num;
+//   update cust_acc set balance = @receiver_bal + amount where acc_num = receiver_acc_num;
+
+//   set isSuccessful = 1;
+
+//   insert into cust_trans (cust_trans.sender_balance_snapshot,
+//                             cust_trans.receiver_balance_snapshot,
+//                             cust_trans.trans_status,
+//                             cust_trans.sender_acc_num,
+//                             cust_trans.receiver_acc_num, cust_trans.amount)
+//                             VALUES (@sender_bal - amount, @receiver_bal + amount, isSuccessful, sender_acc_num,receiver_acc_num, amount);
+//   commit;
+
+// END$$
+// DELIMITER ;
